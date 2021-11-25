320647d8cc79d83effa2e29aec3eb831bcd7e314
yuhara327 <51362368+Yuhara327@users.noreply.github.com>
2021-11-25 20:27:10 +0900

    add escapeSequences

---
+++ escapeSequences.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void) {
+	printf("私の名前は%sです。年齢は%d歳です\n","山田太郎",20);//%s:文字列,%d:整数値
+	printf("イニシャルは%c\n",'Y');//%c:文字
+	printf("%f + %f = %f\n",1.2,2.7,1.2 + 2.7);//%f:実数値
+}
